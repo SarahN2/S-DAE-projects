1b71aed1c430ebff8aff2d90c69b1b273b3d4fb6
SarahN2 <nakhisarah8@gmail.com>
2024-10-22 16:53:13 -0400

    This is my second submission

---
+++ python_1/first.py
@@ -0,0 +1,19 @@
+# variable is like a container  for a value 
+number1=10
+
+print(number1)
+a1=30
+b2=40
+c3=50
+print(a1,b2,c3)
+int1=7
+int2=14
+int3=98
+print(int1,int2,int3) 
+text= "bye bye"
+print(text)
+char1=7
+char2=14
+char3=98
+print(char1,char2,char3)
+
